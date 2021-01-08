01919aebcf0f059af0b263a8250f1b2b3db223f7
Martin Danek <martindanek4@gmail.com>
2021-01-08 23:40:30 +0100

    statistics and result_dict

--- temperature_analyzer.py
+++ temperature_analyzer.py
@@ -1,6 +1,7 @@
 # avarage temperature from citytemp.csv
 import csv
 from pprint import pprint as pp
+import statistics
 
 math_dict = {}
 
@@ -21,30 +22,54 @@ def build_dict(data_list):
         if item.isdigit() and data_list[index + 1].startswith('F'):
             data_list[index + 1] = data_list[index + 1][1:]
             data_list.insert(index + 1, 'F')
-            data_dict = {data_list[index - 1]: {data_list[index + 1]: []}}
         elif item.isdigit() and data_list[index + 1].startswith('C'):
             data_list[index + 1] = data_list[index + 1][1:]
             data_list.insert(index + 1, 'C')
-            data_dict = {data_list[index - 1]: {data_list[index + 1]: []}}
 
-# if index % 3 == 0 and index != 0:
-# data_dict[data_list[index - 3]].get(data_list[index - 1]).append(int(data_list[index - 2]))
-
+        while index % 3 == 0 and index != 0:
+            if data_list[index - 3] not in data_dict:
+                data_dict.update({data_list[index - 3]: {data_list[index - 1]: [data_list[index - 2]]}})
+            elif data_list[index - 1] not in data_dict[data_list[index - 3]]:
+                data_dict[data_list[index - 3]].update({data_list[index - 1]: [data_list[index - 2]]})
+            else:
+                temp_list = data_dict[data_list[index - 3]][data_list[index - 1]]
+                temp_list.append(data_list[index - 2])
+            break
     return data_dict
 
 
-def math_oper(data_dict):
+def stat_oper(data_dict):
+    # [°C] = ([°F] − 32) × 5⁄9
+    # [°F] = [°C] × 9⁄5 + 32
+    result_dict = {}
+    city_list = [city for city in data_dict]
+    for city in city_list:
+        if len(data_dict[city]) > 1:
+            temp_list = [convert_f_c(int(temp)) for temp in data_dict[city]['F']]\
+                            .append([int(temp) for temp in data_dict[city]['C']])
+            print(temp_list)
+        elif 'F' in data_dict[city]:
+            temp_list = [convert_f_c(int(temp)) for temp in data_dict[city]['F']]
+        else:
+            temp_list = [int(temp) for temp in data_dict[city]['C']]
+            avg = statistics.mean(temp_list)
+            print(temp_list, avg)
+    return result_dict
 
-    return math_dict
+
+def convert_f_c(temp):
+    temp = (temp - 32) * 5/9
+    return temp
 
 
 def main():
     data_list = data_from_csv()
     data_dict = build_dict(data_list)
+    result_dict = stat_oper(data_dict)
 
     print(data_list)
     pp(data_dict)
-    pp(math_dict)
+    pp(result_dict)
 
 
 main()
