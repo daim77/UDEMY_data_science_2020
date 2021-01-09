b1bae4560b7e2a2a95c309fe06a6497f3bc20f2d
Martin Danek <martindanek4@gmail.com>
2021-01-09 20:01:10 +0100

    so so done

--- temperature_analyzer.py
+++ temperature_analyzer.py
@@ -1,6 +1,5 @@
 # avarage temperature from citytemp.csv
 import csv
-from pprint import pprint as pp
 import statistics
 
 math_dict = {}
@@ -41,20 +40,23 @@ def build_dict(data_list):
 def stat_oper(data_dict):
     # [°C] = ([°F] − 32) × 5⁄9
     # [°F] = [°C] × 9⁄5 + 32
-    result_dict = {}
+    result_dict = {'city name': ['average temp', 'median temp']}
     city_list = [city for city in data_dict]
     for city in city_list:
         if len(data_dict[city]) > 1:
-            temp_list = [convert_f_c(int(temp)) for temp in data_dict[city]['F']]\
-                            .append([int(temp) for temp in data_dict[city]['C']])
-            print(temp_list)
+            temp_list = [convert_f_c(int(temp)) for temp in data_dict[city]['F']]
+            for num in [int(temp) for temp in data_dict[city]['C']]:
+                temp_list.append(num)
         elif 'F' in data_dict[city]:
             temp_list = [convert_f_c(int(temp)) for temp in data_dict[city]['F']]
         else:
             temp_list = [int(temp) for temp in data_dict[city]['C']]
-        print(city, temp_list)
-        # avg = statistics.mean(temp_list)
-        # result_dict.update({city: avg})
+
+        result_dict.update(
+            {city: [
+                round(statistics.mean(temp_list), 1),
+                round(statistics.median(temp_list), 1)
+            ]})
     return result_dict
 
 
@@ -63,14 +65,19 @@ def convert_f_c(temp):
     return temp
 
 
+def output(result_dict):
+    print('\nAll temperature in Ceslisu\n' + '=' * 60)
+    for city in result_dict:
+        print('{:^30}'.format(city))
+        for temp in result_dict[city]:
+            print(temp)
+
+
 def main():
     data_list = data_from_csv()
     data_dict = build_dict(data_list)
     result_dict = stat_oper(data_dict)
-
-    # print(data_list)
-    # pp(data_dict)
-    # pp(result_dict)
+    output(result_dict)
 
 
 main()
